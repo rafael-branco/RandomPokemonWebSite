0f36ea75eda40fb260a445867fa0895d0c8ada4f
rafael-branco <rafael.rbranco@hotmail.com>
2021-10-11 15:38:32 -0300

    Update

--- getDataFromAPI.py
+++ getDataFromAPI.py
@@ -7,6 +7,7 @@ if not os.path.exists("JSON/allPokemons.json"):
     with open('JSON/allPokemons.json', 'w') as fp:
         json.dump(response, fp)
     print("General file created!")
+    response.close()
 else:
     print("General file already exists!")
 
@@ -18,7 +19,19 @@ total = len(general_data['results'])
 
 pokemons = general_data['results']
 
-print(pokemons[0])
+for p in pokemons:
+    
+    temp_url = p["url"]
+    folder = temp_url.split("/")[-2]
+
+    if not os.path.exists("JSON/Pokemons/"+ folder +".json"):
+        rsp = requests.get(str(temp_url)).json()
+        with open("JSON/Pokemons/"+ folder +".json", 'w') as fp:
+            json.dump(rsp, fp)
+            print(folder+" file created!")
+    else:
+        print(folder + " file already exists!")
+
 
 
 
