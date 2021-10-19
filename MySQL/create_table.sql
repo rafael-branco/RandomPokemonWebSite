02041b217ea085edbcf2d26d357a297dff656746
Rafael <rafael.rbranco@hotmail.com>
2021-10-18 22:44:35 -0300

    Update create_table.sql

--- MySQL/create_table.sql
+++ MySQL/create_table.sql
@@ -1,9 +1,9 @@
-create database if not exists pokebase;
-use pokebase;
-create table  if not exists pokemon(
+CREATE database if NOT EXISTS pokebase;
+USE pokebase;
+CREATE TABLE  if NOT EXISTS pokemon(
 	id INT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    type01 varchar(255) NOT NULL,
-    type02 varchar(255),
-    sprite varchar(300) NOT NULL
+    type01 VARCHAR(255) NOT NULL,
+    type02 VARCHAR(255),
+    sprite VARCHAR(300) NOT NULL
 );
