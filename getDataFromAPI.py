546fda6a6e6d6e9c46ae8a83eec097d2d66e3add
Rafael <rafael.rbranco@hotmail.com>
2021-10-18 22:39:51 -0300

    Update getDataFromAPI.py

--- getDataFromAPI.py
+++ getDataFromAPI.py
@@ -3,6 +3,7 @@ import json
 import os
 import time
 import mysql.connector
+from time import sleep
 
 if not os.path.exists("JSON/allPokemons.json"):
     response = json.loads(requests.get("https://pokeapi.co/api/v2/pokemon?limit=2000").text)
@@ -16,10 +17,10 @@ else:
 mysql_file = open('JSON/mysql.json',)
 mysql_data = json.load(mysql_file)
 
-mydb = mysql.connector.connect(user=mysql_data['mysql_info']['user'],
-                              password=mysql_data['mysql_info']['password'],
-                              host=mysql_data['mysql_info']['host'],
-                              database=mysql_data['mysql_info']['database'])
+mydb = mysql.connector.connect(user=mysql_data['mysql_info'][0]['user'],
+                              password=mysql_data['mysql_info'][0]['password'],
+                              host=mysql_data['mysql_info'][0]['host'],
+                              database=mysql_data['mysql_info'][0]['database'])
 db_cursor = mydb.cursor()
 
 general_file = open('JSON/allPokemons.json',)
@@ -29,6 +30,8 @@ general_data = json.load(general_file)
 total = len(general_data['results'])
 
 pokemons = general_data['results']
+
+qtd = []
 
 for p in pokemons:
     
@@ -42,15 +45,24 @@ for p in pokemons:
             print(folder+" file created!")
             time.sleep(5)
     else:
-        print(folder + " file already exists!")
+        #print(folder + " file already exists!")
         pokefile = open("JSON/Pokemons/" + folder + ".json", )
         pokemon = json.load(pokefile)
         sql = "INSERT IGNORE INTO pokemon (id, name, type01, type02, sprite) VALUES (%s, %s, %s, %s, %s)"
-        val = (pokemon['id'], pokemon['name'], pokemon['type01'], pokemon['type02'],  pokemon['sprite'])
+        count = 0
+        types = []
+        for type in pokemon['types']:
+            types.append(type['type']['name'])
+
+        if len(types) == 1:
+            types.append(None)
+
+        val = (pokemon['id'], pokemon['name'], types[0], types[1], pokemon['sprites']['other']['official-artwork']['front_default'])
         db_cursor.execute(sql, val)
         mydb.commit()
+        pokefile.close()
 
-
+print("Done!")
 
 
 
