ddeb3a4329c7ea9418ef06e5534c75624ec02b66
Rafael <rafael.rbranco@hotmail.com>
2021-10-22 03:05:18 -0300

    Update

---
+++ getRandomPokemon.py
@@ -0,0 +1,18 @@
+import json
+import mysql.connector
+from time import sleep
+
+def main():
+    print("ABC")
+    '''
+    mysql_file = open('JSON/mysql.json',)
+    mysql_data = json.load(mysql_file)
+
+    mydb = mysql.connector.connect(user=mysql_data['mysql_info'][0]['user'],
+                                password=mysql_data['mysql_info'][0]['password'],
+                                host=mysql_data['mysql_info'][0]['host'],
+                                database=mysql_data['mysql_info'][0]['database'])
+    db_cursor = mydb.cursor()
+    '''
+    
+
