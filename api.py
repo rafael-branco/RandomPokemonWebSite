ec4e9956ff7b96010b5a04a6dcf34c7b681b969e
Rafael <rafael.rbranco@hotmail.com>
2022-02-10 19:31:48 -0300

    Update

---
+++ api.py
@@ -0,0 +1,23 @@
+from flask import *
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home_page():
+    data_set = {'Page': 'Home', 'Message': 'Hello World! :D'}
+    json_dump = json.dumps(data_set)
+    return json_dump
+
+    
+@app.route('/user/', methods=['GET'])
+def request_page():
+
+    user_query = str(request.args.get('user')) 
+
+    data_set = {'Page': 'Request', 'Message': f'Hello, {user_query}! :D'}
+    json_dump = json.dumps(data_set)
+    return json_dump
+
+if(__name__ == '__main__'):
+    app.run(port=7777)
