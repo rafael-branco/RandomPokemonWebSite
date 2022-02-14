e866c03213c5734df5b5e2ff949583cda17984be
Rafael <rafael.rbranco@hotmail.com>
2022-02-14 01:56:14 -0300

    Update app.py

--- API/app.py
+++ API/app.py
@@ -4,12 +4,11 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/rafa', methods=['GET'])
 def home_page():
     data_set = {'Page': 'Home', 'Message': 'Hello World! :D'}
     json_dump = json.dumps(data_set)
     return json_dump
 
 if __name__=="__main__":
-    app.run(host=os.getenv('IP', '0.0.0.0'), 
-            port=int(os.getenv('PORT', 4444)))
+    app.run()
